b3e060e24b53369d7ff1c15755e4b2cada832c8a
Derrick Hathaway <derrick.hathaway@lifeomic.com>
2020-12-14 16:29:41 -0700

    restore .d.ts types

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,7 @@
 import configurePrim from './configurePrim'
 import defaultOptions from './defaultOptions'
 
+export { default as configurePrim } from './configurePrim'
+export { default as defaultOptions } from './defaultOptions'
+
 export const { PrimProvider, usePrim, primmed } = configurePrim(defaultOptions)
