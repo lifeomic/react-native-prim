b6d94ca27cb35f1beca511501575ab2544cf0c44
Derrick Hathaway <derrick.hathaway@lifeomic.com>
2020-12-15 16:00:33 -0700

    bob building

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   clearMocks: true,
   preset: 'react-native',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  modulePathIgnorePatterns: ['<rootDir>/lib/'],
   testPathIgnorePatterns: ['dist/', 'utils/'],
   setupFilesAfterEnv: ['<rootDir>/jest.afterEnv.js'],
   collectCoverageFrom: ['src/**/*.{ts,tsx}'],
